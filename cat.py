41738ae0c57a560dd2de51fcf90e6e1cfb40ab26
Jessica Perchard <jessicaperchard@Jessicas-iMac-2.local>
2020-09-09 17:59:20 +0100

    cat

---
+++ cat.py
@@ -0,0 +1,32 @@
+#Exercise 1
+
+animal = 'dog'
+vegetable = 'carrot'
+mineral = 'salt'
+
+print('Here is an animal, a vegetable, and a mineral.')
+print(animal)
+print(vegetable)
+print(mineral)
+
+
+#Exercise 2
+
+name = input('What is your name? ')
+print('You entered: ', name)
+
+
+#Exercise 3
+
+cat_speak = input('What does a cat say? ')
+print(cat_speak)
+top_line = '_'*20
+
+print('{0:>30}'.format(top_line))
+print('{0:>9}  {1:>1}{2}{3:>18}' .format('< ', cat_speak,'!', '>'))
+print('{0:>30}'.format(top_line))
+
+print('{0:>9}'.format('/'))
+print(' /\_/\ ''{0:1}'.format('/'))
+print('( o.o )')
+print(' > ^ < ')
